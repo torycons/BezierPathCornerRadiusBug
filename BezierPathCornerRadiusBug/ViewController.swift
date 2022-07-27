42ba86b1d2295286eccc2cbeb1157ac3b6cdd58c
Thanapat Sorralump <thanapat423@gmail.com>
2022-07-27 13:39:52 +0700

    Add views

--- BezierPathCornerRadiusBug/ViewController.swift
+++ BezierPathCornerRadiusBug/ViewController.swift
@@ -7,13 +7,55 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController {}
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+final class CornerViewGreen: UIView {
+    override func draw(_ rect: CGRect) {
+        let roundedPath = UIBezierPath(
+            roundedRect: CGRect(
+                x: bounds.origin.x,
+                y: bounds.origin.y,
+                width: bounds.width,
+                height: bounds.height),
+            byRoundingCorners: [.allCorners],
+            cornerRadii: CGSize(
+                width: 25,
+                height: 25))
+        UIColor.green.setFill()
+        roundedPath.fill()
     }
-
-
 }
 
+final class CornerViewBlue: UIView {
+    override func draw(_ rect: CGRect) {
+        let roundedPath = UIBezierPath(
+            roundedRect: CGRect(
+                x: bounds.origin.x,
+                y: bounds.origin.y,
+                width: bounds.width,
+                height: bounds.height),
+            byRoundingCorners: [.allCorners],
+            cornerRadii: CGSize(
+                width: 30,
+                height: 30))
+        UIColor.blue.setFill()
+        roundedPath.fill()
+    }
+}
+
+final class CornerViewRed: UIView {
+    override func draw(_ rect: CGRect) {
+        let roundedPath = UIBezierPath(
+            roundedRect: CGRect(
+                x: bounds.origin.x,
+                y: bounds.origin.y,
+                width: bounds.width,
+                height: bounds.height),
+            byRoundingCorners: [.allCorners],
+            cornerRadii: CGSize(
+                width: 35,
+                height: 35))
+        UIColor.red.setFill()
+        roundedPath.fill()
+    }
+}
